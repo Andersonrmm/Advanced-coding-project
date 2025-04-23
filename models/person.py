1a5c70c34204ee82910185edeada68a5b1acd01e
Andersonrmm <machavaanderson8@gmail.com>
2025-04-23 12:06:34 +0200

    definng new class and addition of comments

--- models/person.py
+++ models/person.py
@@ -5,7 +5,8 @@ class person:
 
 class user(person):
     def __init__(self, person_id, name):
-        person.__init__(self, person_id, name)
+        person.__init__(self, person_id, name) # For user class to reuse the code from person class 
         self.plan = []
 
-    
+    def put_plan(self,plan):
+        self.plans.append(plan) # To add the plan to the given plans list 
