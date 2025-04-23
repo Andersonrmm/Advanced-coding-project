0319bf4dd963c122ecd9f920a66ddec314375aaf
Andersonrmm <machavaanderson8@gmail.com>
2025-04-23 11:52:48 +0200

    Inheritance of class user from person

--- models/person.py
+++ models/person.py
@@ -2,4 +2,10 @@ class person:
     def __init__(self, person_id, name):
         self.person_id = person_id
         self.name = name
+
+class user(person):
+    def __init__(self, person_id, name):
+        person.__init__(self, person_id, name)
+        self.plan = []
+
     
