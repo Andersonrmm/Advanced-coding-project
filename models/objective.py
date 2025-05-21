fcb149ce1d6abfaff9828213f7bc389abd4c45c1
Andersonrmm <machavaanderson8@gmail.com>
2025-05-21 11:51:53 +0200

    creation of menu

--- models/objective.py
+++ models/objective.py
@@ -1,9 +1,9 @@
 import datetime
 
 class objective: 
-    def __init__(self, topic, objective_duration, due_date):
+    def __init__(self, topic, expected_hours, due_date):
         self.topic = topic # Name of the objective to be completed
-        self.expected_hours = objective_duration # How many hours needed for the objective to be completed 
+        self.expected_hours = expected_hours # How many hours needed for the objective to be completed 
         self.due_date = due_date 
         self.hours_accomplished = 0 # The total amount of the hours done so far
         self.history = [] # keeps a record of the dates and hours 
@@ -13,5 +13,5 @@ class objective:
         self.history.append((datetime.date.today(), hours)) # Log with the present date and the hours worked
 
     def view_progression(self): 
-        return (self.hours_accomplished / self.objective_duration) * 100 # To calculate the percentage completed of the objective
+        return (self.hours_accomplished / self.expected_hours) * 100 # To calculate the percentage completed of the objective
     
