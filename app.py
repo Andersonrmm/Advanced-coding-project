0ae81cf68310b5bb526ae46b4d0c1070318c3ef1
Andersonrmm <machavaanderson8@gmail.com>
2025-05-28 14:47:29 +0200

    app creation

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-import tkinter
+import tkinter as tk
 from tkinter import messagebox
 from tkinter.simpledialog import askstring
 from models.person import person
 from models.objective import objective
+from persistence import save_person, read_person
+import datetime
 
+person = read_person()
+if not person:
+    name = askstring ("Greetings", "Kindly provide your name")
+    person = person(person_id=1, name=name)
+
+root = tk.Tk() 
+root.titte = ("Objectives Planner")
+    
 
