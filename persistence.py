6e34529d8018e573bf4372deb49472875daad196
Andersonrmm <machavaanderson8@gmail.com>
2025-05-28 12:52:46 +0200

    save and read/load system using pickle

--- persistence.py
+++ persistence.py
@@ -6,3 +6,9 @@ DATA_FILE = "person_data.pkl"
 def save_person(person):
     with open(DATA_FILE, "wb") as f:
         pickle.dump(person, f)
+
+def read_person():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "rb") as f:
+            return pickle.load(f)
+    return None
