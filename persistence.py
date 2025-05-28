df46399ecbb2445109b08a6edadfb341ea47ac8c
Andersonrmm <machavaanderson8@gmail.com>
2025-05-28 13:23:41 +0200

    comments

--- persistence.py
+++ persistence.py
@@ -4,11 +4,11 @@ import os
 DATA_FILE = "person_data.pkl"
 
 def save_person(person):
-    with open(DATA_FILE, "wb") as f:
-        pickle.dump(person, f)
+    with open(DATA_FILE, "wb") as f: # To open the file in wb (write binary) mode 
+        pickle.dump(person, f) # Converts the person object and saves it to the file
 
 def read_person():
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, "rb") as f:
-            return pickle.load(f)
+    if os.path.exists(DATA_FILE): # Sees if the file is existent.
+        with open(DATA_FILE, "rb") as f: # To open the file in rb (read binary) mode
+            return pickle.load(f) # Takes the object from the file, breaks it down, and returns it.
     return None
